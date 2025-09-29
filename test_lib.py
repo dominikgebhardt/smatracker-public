a59036c1c91434b2a24241a254efce6f478a410f
dominikgebhardt <g_dominik@web.de>
2025-09-29 21:35:13 +0200

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -1,11 +1,18 @@
 import logging
 from pyspark.sql import SparkSession
 
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 
-logging.info("Trying to import mylib")
-try:
-    from mylib import hello
-    logging.info(hello("World"))
-    logging.info("Successfully imported mylib")
-except ImportError:
-    logging.error("Failed to import mylib")
+# Test logging messages
+logging.info("This is an info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+
+# logging.info("Trying to import mylib")
+# try:
+#   from mylib import hello
+#    logging.info(hello("World"))
+#    logging.info("Successfully imported mylib")
+# except ImportError:
+#    logging.error("Failed to import mylib")
